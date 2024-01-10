f0470f7f845cc6fd3ce2b10ecbc7620c20e76f82
Weicheng Ao <weicheng.ao@outlook.com>
2024-01-10 23:33:53 +0000

    Test Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'mainHelpers.dart';
 import 'navigation.dart';
 
@@ -78,6 +83,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     String greeting = getGreeting();
     Size size = MediaQuery.of(context).size;
+    int currentPageIndex = 0;
+    Color color_sec = Theme.of(context).colorScheme.primaryContainer;
+    final Completer<GoogleMapController> _controller =
+    Completer<GoogleMapController>();
+
+    const CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -85,16 +99,46 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        height: 70,
+        backgroundColor: color_sec,
+        surfaceTintColor: color_sec,
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('1'),
+              child: Icon(Icons.messenger_sharp),
+            ),
+            label: 'Messages',
+          ),
+        ],
+      ),
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        backgroundColor: color_sec,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(systemNavigationBarColor: Theme.of(context).colorScheme.background,
-            statusBarColor: Theme.of(context).colorScheme.primaryContainer),
+        systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(systemNavigationBarColor: color_sec,
+            statusBarColor: color_sec),
       ),
       body: ListView( // Wrap your main content with a ListView
         padding: const EdgeInsets.symmetric(vertical: 8),
@@ -116,10 +160,17 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            // GoogleMap(
+            //   mapType: MapType.hybrid,
+            //   initialCameraPosition: _kGooglePlex,
+            //   onMapCreated: (GoogleMapController controller) {
+            //     _controller.complete(controller);
+            //   },
+            // ),
             SizedBox(
                 height: size.height,
                 width: size.height,
-                child: const NavigationExample()
+                child: MapSample()
             ),
           ],)
 
@@ -133,6 +184,56 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  late GoogleMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    final Brightness brightnessValue = MediaQuery.of(context).platformBrightness;
+    bool isDark = brightnessValue == Brightness.dark;
+
+    // Apply the system theme to the status bar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: isDark ? Brightness.light : Brightness.dark,
+      statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
+    ));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Example'),
+      ),
+      body: Theme(
+        // Use Theme widget to dynamically switch between light and dark themes
+        data: isDark ? ThemeData.dark() : ThemeData.light(),
+        child: GoogleMap(
+          onMapCreated: (controller) {
+            mapController = controller;
+          },
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.7749, -122.4194),
+            zoom: 15.0,
+          ),
+          gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+            Factory<OneSequenceGestureRecognizer>(
+                  () => EagerGestureRecognizer(),
+            ),
+          ].toSet(),
+          myLocationEnabled: true,
+          tiltGesturesEnabled: true,
+          zoomGesturesEnabled: true,
+          scrollGesturesEnabled: true,
+        ),
+      ),
     );
   }
 }
